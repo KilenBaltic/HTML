f17e3fb247320a0a59449800edba1444681d14d1
KilenBaltic <148646201+KilenBaltic@users.noreply.github.com>
2023-10-27 16:06:33 +0300

    header and footer automation

---
+++ footer.php
@@ -0,0 +1,105 @@
+
+
+<section class="content-area prefooter">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <article class="table-content animated" data-fx="flipInY">
+          <section class="table-row">
+            <div class="table-cell">
+              <h1 class="widget-title">Seko mums</h1>
+            </div>
+            <div class="table-cell">
+              <ul class="socialIcons bigIcons">
+                <li><a href="https://www.facebook.com/profile.php?id=100085533383307" target="_blank" data-toggle="tooltip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.linkedin.com/company/kilenbaltic/" target="_blank" data-toggle="tooltip" title="LinkedIn"><i class="fa fa-linkedin"></i></a></li>
+              </ul>
+            </div>
+          </section>
+        </article>
+      </div>
+      
+    </div>
+  </div>
+</section>
+<!-- / section -->
+
+<footer>
+  <div class="container mainfooter">
+    <div class="row">
+      <aside class="col-md-3 widget">
+        <img src="images/kilenbaltic/Optimized/Kilen_Baltic_logo-.png" alt=" ">
+        <br><br>
+
+        <p class="darker">Made with love by Kilen Baltic © 2023</p>
+        <p style="color: rgb(192, 192, 192); font-style: italic;">Umanību: Mājas lapa ir izstrādes procesā un sadaļu saturs var būt nepilnīgs, kā arī saites un sadaļas var būt neaktīvas</p>
+      </aside>
+      <aside class="col-md-3 widget">
+        <h1 class="widget-title">KONTAKTI</h1>
+
+        <a href="mailto:info@kilenbaltic.lv" class="larger">info@kilenbaltic.lv</a>
+
+        <p>
+          Paleju iela 8, Ikšķile, Ogres nov.<br>
+          LV-5052, Latvija
+        </p>
+
+        <p>
+          Tel: +371 27 044 414
+        </p>
+
+      </aside>
+      <aside class="col-md-3 widget">
+        <h1 class="widget-title">Facebook lapa</h1>
+        <div class="fb-page" data-href="https://www.facebook.com/profile.php?id=100085533383307" data-tabs="timeline" data-width="230" data-height="400" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+          <blockquote cite="https://www.facebook.com/profile.php?id=100085533383307" class="fb-xfbml-parse-ignore">
+          <a href="https://www.facebook.com/profile.php?id=100085533383307">Kilen Baltic</a></blockquote>
+        </div>
+      </aside>
+      <aside class="col-md-3 widget">
+        <h1 class="widget-title">Sadaļas</h1>
+        <ul style="list-style: none;">
+          <li><a href="#" class="custom-link">Par mums</a></li>
+          <li><a href="#" class="custom-link">Nekustamo īpašumu projekti</a></li>
+          <li><a href="#" class="custom-link">Dizaina nodaļa</a></li>
+          <li><a href="#" class="custom-link">Privātuma politika</a></li>
+        </ul>
+      </aside>
+    
+    </div>
+  </div>
+</footer>
+
+
+</div>
+<!-- / boxedWrapper -->
+
+<a href="#" id="toTop"><i class="fa fa-angle-up"></i></a>
+
+
+<script src="js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+<script src="js/detectmobilebrowser.js"></script>
+<script src="js/gmap3.min.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/jquery.ba-bbq.min.js"></script>
+<script src="js/jquery.countTo.js"></script>
+<script src="js/jquery.fitvids.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.mb.YTPlayer.js"></script>
+<script src="js/jquery.placeholder.min.js"></script>
+<script src="js/retina-1.1.0.min.js"></script>
+<script src="js/timeline/js/storyjs-embed.js"></script>
+<script src="form/js/form.js"></script>
+<script src="twitter/js/jquery.tweet.js"></script>
+
+<script src="js/main.js"></script>
+<script src="js/less.min.js"></script>
+
+
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--[if IE 8]>
+<html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+  <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PX8J5YQ1DC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PX8J5YQ1DC');
+</script>
+  <meta charset="utf-8">
+  <meta name="description" content="Dzelzsbetona elementu ražošana. Mūsu dzelzsbetona elementi ir augstas kvalitātes un piemēroti dažādu veidu konstrukcijām, tostarp industriālām un dzīvojamām ēkām.">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+
+  <title>Kilen Baltic - dzelzsbetona paneļu ražotne</title>
+  
+  <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <script src="js/modernizr.custom.js"></script>
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="bootstrap/js/html5shiv.js"></script>
+  <script src="bootstrap/js/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+<body class="withAnimation">
+  <div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v18.0" nonce="pU39gSfb"></script>
+<div id="boxedWrapper">
+
+<!-- navbar -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html"><img src="images/kilenbaltic/Optimized/Kilen_Baltic_logo-.jpg" alt="Kilen Baltic logo" style="height:70px;"></a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <form class="pull-right header-search" role="form" style="display:none;">
+        <fieldset>
+          <div class="container">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Type to search...">
+            </div>
+            <button type="submit"><i class="fa fa-search"></i></button>
+          </div>
+        </fieldset>
+      </form>
+      <a href="#" id="showHeaderSearch" class="hidden-xs"><i class="fa fa-search"></i></a>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="index.html">SĀKUMS</a></li>
+        <li class="dropdown">
+          <a href="index.html" class="dropdown-toggle" data-toggle="dropdown" data-target="#">PAR MUMS</a>
+          <ul class="dropdown-menu">
+            <li><a href="index.html">Par uzņēmumu</a></li>
+            <li><a href="index.html">Izstrādājumi</a></li>
+            <li><a href="index.html">Sertifikāti</a></li>
+            <li><a href="index.html">Vakances</a></li>
+            <li><a href="index.html">Dizaina nodaļa</a></li>
+            <li><a href="index.html">Nekustamo īpašumu projekti</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="index.html" class="dropdown-toggle" data-toggle="dropdown" data-target="#">Produkcija</a>
+          <ul class="dropdown-menu">
+            <li><a href="index.html">Portfolio</a></li>
+            <li><a href="index.html">Standarta produkti</a></li>
+            <li><a href="index.html">Sertifikāti</a></li>
+            <li><a href="index.html">Cenas</a></li>
+          </ul>
+        </li>
+        <li><a href="index.html">Blogs</a></li>
+        <li><a href="07-pluto-contact.html">KONTAKTI</a></li>
+        <li><a href="07-pluto-contact.html" style="color:rgb(248, 126, 126);" >EN</a></li>
+        <li><a href="07-pluto-contact.html" style="color:rgb(248, 126, 126);" >SE</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- / navbar -->
